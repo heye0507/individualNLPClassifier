385a2d10e3a697a7224791df58bf6259f4eb45d2
Hao <heye0507@gmail.com>
2019-10-08 06:33:20 +0000

    add downloader support to download pre-trained weights from google drive

--- runner.py
+++ runner.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 from clasi_user.trainer import *
+from clasi_user.downloader import *
+import os
 import argparse
 
 if __name__ == "__main__":
@@ -22,12 +24,21 @@ if __name__ == "__main__":
         dest="lm_vocab_path",
         type=str,
         help="The directory that contains language data vocab",
-        default='/home/jupyter/insight_project/Project-M/data/preprocessed/csv/models'
+        default=None
     )
 
     args = parser.parse_args()
     output_filepath = Path(args.input_file).ls()
     test = Interface(output_filepath,eval_mode=True)
-    test.pre_processing(test=True,lm_path=args.lm_vocab_path)
-    test_train = Trainer(test)
+    if args.lm_vocab_path:
+        test.pre_processing(test=True,lm_path=args.lm_vocab_path)
+    else:
+        print('downloading vocab...')
+        path = Path(args.input_file)/'models'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        download_file_from_google_drive(data_lm_large_id,path/'data_lm_large.pkl')
+        download_file_from_google_drive(model_id,path/'general-clasifier-0.84.pth')
+        test.pre_processing(test=True,lm_path=path)
+    test_train = Trainer(test,path)
     test_train.train_individual_clasifier()
